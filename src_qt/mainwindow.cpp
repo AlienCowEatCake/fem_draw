9403eb6cc7195b92915db597c5a325c3e0ea9791
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-06-20 13:04:10 +0700

    Сменил версию на beta3

--- src_qt/mainwindow.cpp
+++ src_qt/mainwindow.cpp
@@ -330,7 +330,7 @@ void MainWindow::on_actionAbout_FEM_Draw_triggered()
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.setDefaultButton(QMessageBox::Ok);
     msgBox.setWindowTitle("About");
-    msgBox.setText(trUtf8("<b>FEM Draw v1.0 beta2 (Qt)</b><br><br>"
+    msgBox.setText(trUtf8("<b>FEM Draw v1.0 beta3 (Qt)</b><br><br>"
                           "<a href=\"http://fami-net.dlinkddns.com/gitlab/peter/fem_draw\">http://fami-net.dlinkddns.com/gitlab/peter/fem_draw</a><br>"
                           "License: <a href=\"http://www.gnu.org/copyleft/gpl.html\">GNU GPL v3</a><br><br>"
                           "Copyright &copy; 2014 - 2015<br>"
