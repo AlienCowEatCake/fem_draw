8edc21da3e782297d25296dc6738ceaff667f563
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-08-12 01:30:49 +0600

    Сменил версию на 1.2 beta1

--- src_winapi/main.cpp
+++ src_winapi/main.cpp
@@ -550,7 +550,7 @@ void on_actionAbout_FEM_Draw_triggered()
 
     // Надпись "FEM Draw <version_name> (WinAPI)"
     CreateWindow(
-                WC_STATIC, TEXT("FEM Draw v1.1 (WinAPI)"),
+                WC_STATIC, TEXT("FEM Draw v1.2 beta1 (WinAPI)"),
                 WS_CHILD | WS_VISIBLE | SS_CENTERIMAGE,
                 92, 10, 254, 15,
                 hwnd_about, (HMENU)ABOUT_LABEL_VERSION, hInstance, NULL
