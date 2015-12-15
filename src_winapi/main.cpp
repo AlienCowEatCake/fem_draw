c00165d3fef4775a5082c173595115c73e9228e4
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-12-15 21:24:50 +0600

    Сменил версию на 1.5

--- src_winapi/main.cpp
+++ src_winapi/main.cpp
@@ -563,7 +563,7 @@ void on_actionAbout_FEM_Draw_triggered()
 
     // Надпись "FEM Draw <version_name> (WinAPI)"
     CreateWindow(
-                WC_STATIC, TEXT("FEM Draw v1.3 (WinAPI)"),
+                WC_STATIC, TEXT("FEM Draw v1.5 (WinAPI)"),
                 WS_CHILD | WS_VISIBLE | SS_CENTERIMAGE,
                 92, 10, 205, 15,
                 hwnd_about, (HMENU)ABOUT_LABEL_VERSION, hInstance, NULL
