c78050260dd37ec34e558a7b48efb4984355c1c5
Peter Zhigalov <peter.zhigalov@gmail.com>
2016-03-08 15:28:21 +0600

    Сменил версию на 1.6

--- src_qt/mainwindow.cpp
+++ src_qt/mainwindow.cpp
@@ -562,7 +562,7 @@ void MainWindow::on_actionAbout_FEM_Draw_triggered()
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.setDefaultButton(QMessageBox::Ok);
     msgBox.setWindowTitle("About");
-    msgBox.setText("<b>FEM Draw v1.6 beta1 (Qt)</b><br><br>"
+    msgBox.setText("<b>FEM Draw v1.6 (Qt)</b><br><br>"
                    "<a href=\"https://fami.codefreak.ru/osp/fem_draw/\">https://fami.codefreak.ru/osp/fem_draw/</a><br>"
                    "License: <a href=\"http://www.gnu.org/copyleft/gpl.html\">GNU GPL v3</a><br><br>"
                    "Copyright &copy; 2014-2016<br>"
